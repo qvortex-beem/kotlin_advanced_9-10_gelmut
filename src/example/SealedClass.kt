ea966a57fb90ba88b37c5afcdaa6ed928688580d
qvortex-beem <ghujfeuhh1@gmail.com>
2026-03-24 00:04:30 +0300

    learn sealed-class

---
+++ src/example/SealedClass.kt
@@ -0,0 +1,31 @@
+package example
+
+sealed class NetworkResult {
+    data class Success(val data: String): NetworkResult()
+    data class Error(val message: String, val code: Int) : NetworkResult()
+    object Loading : NetworkResult()
+}
+
+fun handleResult(result: NetworkResult) {
+    when (result) {
+        is NetworkResult.Success -> {
+            println("Успех: ${result.data}")
+        }
+        is NetworkResult.Error -> {
+            println("Ошибка ${result.code}: ${result.message}")
+        }
+        NetworkResult.Loading -> {
+            println("Загрузка...")
+        }
+    }
+}
+
+fun main() {
+    val success = NetworkResult.Success("данные получены")
+    val error = NetworkResult.Error("Сервер не отвечает", 500)
+    val loading = NetworkResult.Loading
+
+    handleResult(success)
+    handleResult(error)
+    handleResult(loading)
+}
